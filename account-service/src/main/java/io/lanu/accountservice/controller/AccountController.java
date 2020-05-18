05c19435050d2b4b6e92577fe573e8bcb0300bdd
mrlanu <mrlanu@gmail.com>
2020-05-17 22:28:38 -0500

    fix: change some configurations

--- account-service/src/main/java/io/lanu/accountservice/controller/AccountController.java
+++ account-service/src/main/java/io/lanu/accountservice/controller/AccountController.java
@@ -4,6 +4,7 @@ import io.lanu.accountservice.dto.UserDto;
 import io.lanu.accountservice.dto.UserRegistrationDto;
 import io.lanu.accountservice.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,11 @@ public class AccountController {
     @PostMapping
     public UserDto createNewAccount(@RequestBody UserRegistrationDto user) {
         return accountService.create(user);
+    }
+
+    @GetMapping
+    public String test(){
+        return "Secret resources";
     }
 
 }
--- auth-service/src/main/java/io/lanu/authservice/config/mongodb/changelogs/InitialValuesChangeLog.java
+++ auth-service/src/main/java/io/lanu/authservice/config/mongodb/changelogs/InitialValuesChangeLog.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @ChangeLog
 public class InitialValuesChangeLog {
 
-    @ChangeSet(order = "001", id = "insertBrowserClientDetails", author = "Marcus Hert Da Corégio")
+    @ChangeSet(order = "001", id = "insertBrowserClientDetails", author = "mr Lanu")
     public void insertBrowserClientDetails(MongoTemplate mongoTemplate) {
         AuthClientDetails browserClientDetails = new AuthClientDetails();
         browserClientDetails.setClientId("browser");
@@ -24,8 +24,8 @@ public class InitialValuesChangeLog {
         mongoTemplate.save(browserClientDetails);
     }
 
-    @ChangeSet(order = "002", id = "insertUserToTestAuthentication", author = "Marcus Hert Da Corégio")
-    public void insertUserToTestAuthentication(MongoTemplate mongoTemplate) {
+    @ChangeSet(order = "002", id = "insertUserToTestAuthentication", author = "mr Lanu")
+    public void insertSecondUserToTestAuthentication(MongoTemplate mongoTemplate) {
         Set<Authorities> authorities = new HashSet<>();
         authorities.add(Authorities.ROLE_USER);
 
@@ -33,15 +33,26 @@ public class InitialValuesChangeLog {
         user.setActivated(true);
         user.setAuthorities(authorities);
         user.setPassword("$2a$10$fWNTd3H.u7G/aNROVQSifebOkZ2xzU5nUPOCI2Ld42M8E25/ljJqK");
-        user.setUsername("randomuser");
+        user.setUsername("mrlanu");
 
         mongoTemplate.save(user);
     }
 
-    @ChangeSet(order = "003", id = "insertAccountServiceClientDetails", author = "Marcus Hert Da Corégio")
+    @ChangeSet(order = "003", id = "insertAccountServiceClientDetails", author = "mr Lanu")
     public void insertAccountServiceClientDetails(MongoTemplate mongoTemplate) {
         AuthClientDetails accountServiceClientDetails = new AuthClientDetails();
         accountServiceClientDetails.setClientId("account-service");
+        accountServiceClientDetails.setClientSecret("$2a$10$fWNTd3H.u7G/aNROVQSifebOkZ2xzU5nUPOCI2Ld42M8E25/ljJqK");
+        accountServiceClientDetails.setScopes("server");
+        accountServiceClientDetails.setGrantTypes("refresh_token,client_credentials");
+
+        mongoTemplate.save(accountServiceClientDetails);
+    }
+
+    @ChangeSet(order = "004", id = "insertEmployeeServiceClientDetails", author = "mr Lanu")
+    public void insertEmployeeServiceClientDetails(MongoTemplate mongoTemplate) {
+        AuthClientDetails accountServiceClientDetails = new AuthClientDetails();
+        accountServiceClientDetails.setClientId("employee-service");
         accountServiceClientDetails.setClientSecret("$2a$10$fWNTd3H.u7G/aNROVQSifebOkZ2xzU5nUPOCI2Ld42M8E25/ljJqK");
         accountServiceClientDetails.setScopes("server");
         accountServiceClientDetails.setGrantTypes("refresh_token,client_credentials");
